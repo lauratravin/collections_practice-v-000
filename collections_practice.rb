d22fd0360f42a03580f6ef6d14db9706c72a1e5f
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 19:49:10 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,8 @@ def sort_array_desc(array)
   newarray = []
   newarray =  array.sort.reverse
 end
+
+def SORT_ARRAY_CHAR_COUNT(array)
+  newarray = []
+  newarray =  array.sort {|a, b| a.length <=> b.length}
+end
